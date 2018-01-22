7f78dc44336477215d25af29da8d350759a61433
puritybirir <purity.birir@andela.com>
2018-01-22 21:57:41 +0300

    Understand how propTypes work

--- app/index.js
+++ app/index.js
@@ -1,51 +1,33 @@
-var React = require("react");
-var ReactDOM = require("react-dom");
-require("./index.css");
+var React = require('react');
+var ReactDOM = require('react-dom');
+var PropTypes = require('prop-types');
 
-//State
-//lifecycle event
-//UI of the component
-
-class App extends React.Component {
-  render() {
-      var friends = this.props.list.filter(function(user){
-        return user.friend ===true
-    })
-      var nonFriends = this.props.list.filter(function(user){
-          return user.friend !==true
-      })
-    return (
-      <div>
-        <h1>Friends</h1>
-        <ul>
-          {friends.map(function(user) {
-              return <li key={user.name}>{user.name}</li>;
-            })}
-        </ul>
-
-        <hr />
-
-        <h1> Non Friends </h1>
-        <ul>
-          {nonFriends.map(function(user) {
-              return <li key={user.name}>{user.name}</li>;
-            })}
-        </ul>
-      </div>
-    );
-  }
+class Badge extends React.Component{
+    render(){
+        return(
+            <div>
+                <img
+                    src={this.props.img}
+                    alt="Avatar"
+                    style={{width:100, height:100}}
+                />
+                <h1>Name: {this.props.name}</h1>
+                <h3>username: {this.props.username}</h3>
+            </div>
+        )
+    }
 }
 
+Badge.propTypes = {
+    img : PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    username : PropTypes.string.isRequired
+}
 ReactDOM.render(
-  <App
-    list={[
-      { name: "Tyler", friend: true },
-      { name: "Ryan", friend: true },
-      { name: "Michael", friend: false },
-      { name: "Mikenzi", friend: false },
-      { name: "Jessica", friend: true },
-      { name: "Dan", friend: false }
-    ]}
-  />,
-  document.getElementById("app")
+    <Badge
+        name="Tyler McGinnis"
+        username="tylermcginnis"
+        img='https://avatars0.githubusercontent.com/u/2933430?v=3&s=460'
+    />,
+    document.getElementById('app')
 );
