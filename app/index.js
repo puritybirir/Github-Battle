6f28df733c32977596546cf4800c3688ab0cb358
puritybirir <purity.birir@andela.com>
2018-01-22 16:55:27 +0300

    Understand how props work in react

--- app/index.js
+++ app/index.js
@@ -1,18 +1,51 @@
-var React = require ('react');
-var ReactDOM = require ('react-dom');
-require('./index.css');
+var React = require("react");
+var ReactDOM = require("react-dom");
+require("./index.css");
 
 //State
 //lifecycle event
 //UI of the component
 
+class App extends React.Component {
+  render() {
+      var friends = this.props.list.filter(function(user){
+        return user.friend ===true
+    })
+      var nonFriends = this.props.list.filter(function(user){
+          return user.friend !==true
+      })
+    return (
+      <div>
+        <h1>Friends</h1>
+        <ul>
+          {friends.map(function(user) {
+              return <li key={user.name}>{user.name}</li>;
+            })}
+        </ul>
 
-class App extends React.Component{
-    render(){
-        return(
-            <div>Hello Dunia</div>
-        )
-    }
+        <hr />
+
+        <h1> Non Friends </h1>
+        <ul>
+          {nonFriends.map(function(user) {
+              return <li key={user.name}>{user.name}</li>;
+            })}
+        </ul>
+      </div>
+    );
+  }
 }
 
-ReactDOM.render(<App />, document.getElementById('app') );
+ReactDOM.render(
+  <App
+    list={[
+      { name: "Tyler", friend: true },
+      { name: "Ryan", friend: true },
+      { name: "Michael", friend: false },
+      { name: "Mikenzi", friend: false },
+      { name: "Jessica", friend: true },
+      { name: "Dan", friend: false }
+    ]}
+  />,
+  document.getElementById("app")
+);
