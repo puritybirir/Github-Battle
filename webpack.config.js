ae5700f0103bee9f508780eea433c74ca04626d3
puritybirir <purity.birir@andela.com>
2018-01-25 16:58:42 +0300

    Add react router to the application

--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,16 @@ module.exports = {
     entry: './app/index',
     output: {
         path:path.resolve(__dirname, 'dist'),
-        filename:'index_bundle.js'
+        publicPath:'/'
     },
     module:{
         rules:[
             { test: /\.js$/, use: 'babel-loader'},
             { test: /\.css$/, use: ['style-loader', 'css-loader']},
         ]
+    },
+    devServer: {
+        historyApiFallback:true
     },
     plugins:[
         new HtmlWebpackPlugin({
