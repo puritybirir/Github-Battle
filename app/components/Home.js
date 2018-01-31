a2577912e40b5be96fcc91ec0f7e4b82f2cfbc99
puritybirir <purity.birir@andela.com>
2018-01-31 17:31:57 +0300

    Fix all bugs and deploy application

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,16 +1,14 @@
-var React = require("react");
-var Link = require("react-router-dom").Link;
+var React = require('react');
+var Link = require('react-router-dom').Link;
 
 class Home extends React.Component {
   render() {
     return (
-      <div className="home-container">
-        <h1>Github Battle: Battle your friends...and stuff.</h1>
-        <Link className="button" to="battle">
-          Battle
-        </Link>
+      <div className='home-container'>
+        <h1>Github Battle: Battle your friends... and stuff.</h1>
+        <Link className='button' to='/battle'>Battle</Link>
       </div>
-    );
+    )
   }
 }
 
